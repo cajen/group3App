035f1a74ccec02a3bea3b6f927b66249824eab11
Christopher <Christopher@news-air.attlocal.net>
2019-02-05 21:49:11 -0800

    Create mongoDB database, host it, and connect to it

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var instrRouter = require('./routes/instruments');
+var mongoose = require('mongoose');
+
+// connect to a mongodb db hosted on mongolab with mongoose
+mongoose.connect('mongodb://cajen:TMbqdz5eMpLshdV@ds123635.mlab.com:23635/gp3db', {useNewUrlParser: true});
+// This is the db reference
+var db = mongoose.connection;
+
+// Test to see if db is connected ... and it is
+
+// db.collection('inventory').insertOne({
+//   item: 'canvas',
+//   qty: 100,
+//   tags: ['cotton'],
+//   size: { h: 28, w: 35.5, uom: 'cm' }
+// });
 
 var app = express();
 
