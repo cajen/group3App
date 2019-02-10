17b732a88e3c33c2ab587cc69f4561e2b0ee3ac4
Christopher <Christopher@news-air.attlocal.net>
2019-02-09 18:52:07 -0800

    Add mongoDB database

--- routes/instruments.js
+++ routes/instruments.js
@@ -1,24 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
 var MongoClient = require('mongodb').MongoClient;
-
 var url = 'mongodb://cajen:TMbqdz5eMpLshdV@ds123635.mlab.com:23635/gp3db';
-
-MongoClient.connect(url,{ useNewUrlParser: true },function(err, client){
-  if(err){
-      console.log(err);
-  }
-  else {
-      var db = client.db('gp3db');
-      console.log('connected to '+ url);
-      client.close();
-  }
-})
 
 /* GET instruments page. */
 router.get('/', function(req, res, next) {
-  res.render('instruments');
+  MongoClient.connect(url,{ useNewUrlParser: true },function(err, client){
+    if(err){
+        console.log(err);
+        res.render('instruments');
+    }
+    else {
+        var db = client.db('gp3db');
+          db.collection('inventory').find().toArray(function (err, result) {
+            let dbDocuments = result;
+            console.log(dbDocuments[0]);
+            res.render('instruments', dbDocuments[0]);
+          });
+        console.log('connected to '+ url);
+        client.close();
+    }
+  });
+
+  // res.render('instruments');
 });
 
 module.exports = router;
