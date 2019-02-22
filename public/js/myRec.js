57749d38b19e3626c5b7d1aef1119dac7bd98ed2
cajen <topher.jensen63@gmail.com>
2019-02-21 21:26:41 -0800

    Recording working

--- public/js/myRec.js
+++ public/js/myRec.js
@@ -1,4 +1,4 @@
-$('.modal').on('show.bs.modal', function(event) {
+$('#mRModal').on('show.bs.modal', function(event) {
 	var RecTitle = $(event.relatedTarget).attr('data-title');
 	var recUrl = $(event.relatedTarget).attr('data-url');
 	var modal = $(this);
